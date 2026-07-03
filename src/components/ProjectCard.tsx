fba7b746c0227b26eb87c2cc7a740c9496e8a814
Kieran <221121806+kieranalmeida@users.noreply.github.com>
2026-07-03 09:32:24 +0100

    Web design page complete

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,22 +9,17 @@ type ProjectCardProps = {
 
 export default function ProjectCard({title, desc, image}: ProjectCardProps): JSX.Element {
     return (
-        <div className="flex flex-col md:flex-row md:h-77.5 2xl:flex-col 2xl:h-full">
-            <img className="md:w-1/2 md:object-cover 2xl:w-full 2xl:h-full" src={image}/>
+        <div className="flex flex-col rounded-2xl md:flex-row md:h-77.5 2xl:flex-col 2xl:h-full">
+            <img className="rounded-t-2xl md:w-1/2 md:object-cover md:rounded-r-none md:rounded-l-2xl 2xl:w-full 2xl:rounded-b-none 2xl:rounded-t-2xl" src={image}/>
                 
-            <Link className="md:w-1/2 2xl:w-full" to="">
+            <Link className="md:w-1/2 rounded-2xl 2xl:w-full" to="">
                 <div 
-                    className="flex flex-col items-center gap-y-4 text-center bg-[#FDF3F0] py-8 px-7.5 md:justify-center md:h-full 2xl:h-39.5 hover:bg-peach"
+                    className="flex flex-col items-center gap-y-4 text-center bg-[#FDF3F0] py-8 px-7.5 rounded-b-2xl group md:justify-center md:h-full md:rounded-l-none md:rounded-r-2xl 2xl:h-39.5 2xl:rounded-t-none 2xl:rounded-b-2xl hover:bg-peach"
                 >
-                    <h2 className="text-peach text-[1.25rem] font-medium leading-6.5 tracking-[5px] uppercase">{title}</h2>
-                    <p className="text-dark-grey leading-6.5">{desc}</p>
+                    <h2 className="text-peach text-[1.25rem] font-medium leading-6.5 tracking-[5px] uppercase group-hover:text-white">{title}</h2>
+                    <p className="text-dark-grey leading-6.5 group-hover:text-white">{desc}</p>
                 </div>
             </Link>
         </div>
     )
 }
-
-// Round corners
-// Check Link focus
-
-// Change text to white on hover
